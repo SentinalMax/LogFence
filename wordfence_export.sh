2817c2016aef526c40db97abe4603eb56bae5bea
Alex Vergara <55516909+SentinalMax@users.noreply.github.com>
2023-08-22 22:33:32 -0500

    made smarter

--- wordfence_export.sh
+++ wordfence_export.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-echo "Whats the name of your MySQL database?"
+echo "Whats the name of your MySQL database:"
 read DATABASE_NAME
 
-FILENAME="outputFile.csv"
+#SELECT 'id','attackLogTime','ctime','ip','jsRun','statusCode','userID','newVisit','URL','referer','UA','action','actionDescription' UNION ALL SELECT id,attackLogTime,ctime,HEX(IP),jsRun,statusCode,userID,newVisit,URL,referer,UA,action,actionDescription FROM wp_wfhits INTO OUTFILE '/var/lib/mysql-files/test8.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+
+echo "Name the output file:"
+read FILENAME
+
+# Concat file ext '.csv' to file name
+EXT=".csv"
+FILENAME=$FILENAME$EXT
 
 MYSQL_QUERY_1="SHOW VARIABLES LIKE \"secure_file_priv\""
 SECURE_FILE_PATH_RAW=$(sudo mysql -uroot -D $DATABASE_NAME -e "$MYSQL_QUERY_1")
 
 PSEUDO_OUTPUT_FILE=$(echo $SECURE_FILE_PATH_RAW | awk '{print $4}')
 OUTPUT_FILE=$PSEUDO_OUTPUT_FILE"$FILENAME"
-echo $OUTPUT_FILE
 
-MYSQL_QUERY_2="TABLE wp_wfhits INTO OUTFILE '$OUTPUT_FILE' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '' LINES TERMINATED BY '\n'"
+# Check if file already exists in path
+if [[ -f "$OUTPUT_FILE" ]]; then
+    echo "$OUTPUT_FILE exists." && exit
+fi
+
+#MYSQL_QUERY_2="TABLE wp_wfhits INTO OUTFILE '$OUTPUT_FILE' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '' LINES TERMINATED BY '\n'"
+MYSQL_QUERY_2="SELECT 'id','attackLogTime','ctime','ip','jsRun','statusCode','userID','newVisit','URL','referer','UA','action','actionDescription' UNION ALL SELECT id,attackLogTime,ctime,HEX(IP),jsRun,statusCode,userID,newVisit,URL,referer,UA,action,actionDescription FROM wp_wfhits INTO OUTFILE '/var/lib/mysql-files/test8.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
 
 sudo mysql -uroot -D $DATABASE_NAME -e "$MYSQL_QUERY_2"
 
